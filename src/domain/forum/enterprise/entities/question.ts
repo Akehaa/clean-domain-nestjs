5812c8ec24bcce700efd50c421dd4eb9646b12b7
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2023-09-11 16:10:54 -0300

    Attachment Entities

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -1,6 +1,5 @@
 import { AggregateRoot } from '@/core/entities/aggregate-root'
 import { Slug } from './value-objects/slug'
-import { Entity } from '@/core/entities/entity'
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
 import dayjs from 'dayjs'
